119fae72e45fcd85a8480a1b0509417ceea60880
Ian Anderson <ian.kirk.anderson@gmail.com>
2023-10-05 22:19:00 +0100

    Get ansible working in the container.

--- .setup.sh
+++ .setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Setup .ssh directory if it does not exist." >>~/setup.log
 if [ ! -d ".ssh" ]; then
     sudo cp -r .ssh-host .ssh
     sudo chown -R developer:developer .ssh
@@ -7,10 +8,10 @@ if [ ! -d ".ssh" ]; then
     chmod 644 .ssh/id_ed25519.pub
 fi
 
-sudo apt-get install git
+sudo apt-get install git -y >>~/setup.log 2>&1
 
 git clone git@github.com:gozumi/development-vm.git >>~/setup.log 2>&1
 
-development-vm/install.sh >>~/setup.log 2>&1
+ansible-playbook development-vm/bootstrap.yml >>~/setup.log 2>&1
 
 sleep infinity
--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,20 @@ FROM ubuntu:lunar
 
 RUN apt-get update
 
-RUN apt-get install adduser
+RUN apt-get install sudo adduser zsh -y
 
 ARG USERNAME=developer
+ARG HOME_DIR=/home/${USERNAME}
+
+RUN apt-get install sudo adduser ansible -y
 
 RUN adduser ${USERNAME}
 
 RUN adduser --disabled-password ${USERNAME} --gecos '' ${USERNAME}
 
 RUN echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+RUN chsh -s $(which zsh)
 
 USER ${USERNAME}
 
