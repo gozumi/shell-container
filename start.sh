b6fea40e5a4348ce7a9099d3a52e93cf1255bd1f
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-07-09 22:03:51 +0100

    Add more automation to the start.sh script.

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 CONTAINER_NAME=shell-development
+CONTAINER_DIR=$HOME/container-development
+
+[ ! -d $CONTAINER_DIR ] && mkdir $CONTAINER_DIR && touch $CONTAINER_DIR/.zsh_history
 
 podman run -it -d \
     --name $CONTAINER_NAME \
@@ -8,11 +11,12 @@ podman run -it -d \
     --memory-reservation=4g \
     -p 2222:22 \
     --cap-add=CAP_AUDIT_WRITE \
-    -v ./.ssh/id_ed25519.pub:/home/developer/.ssh/authorized_keys \
-    -v ./.ssh/id_ed25519.pub:/home/developer/.ssh/id_ed25519.pub \
-    -v ./.ssh/id_ed25519:/home/developer/.ssh/id_ed25519 \
-    -v ~/.gitconfig:/home/developer/.gitconfig \
-    -v ./:/home/developer/development \
+    -v $HOME/.ssh/id_ed25519.pub:/home/developer/.ssh/authorized_keys \
+    -v $HOME/.ssh/id_ed25519.pub:/home/developer/.ssh/id_ed25519.pub \
+    -v $HOME/.ssh/id_ed25519:/home/developer/.ssh/id_ed25519 \
+    -v $HOME/.gitconfig:/home/developer/.gitconfig \
+    -v $CONTAINER_DIR/.zsh_history:/home/developer/.zsh_history \
+    -v $CONTAINER_DIR/:/home/developer/development \
     shell-development:latest \
     /usr/sbin/sshd -D -e
 
