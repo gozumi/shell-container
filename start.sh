5f87c07b03cedd3747f17b7bf15b1f1f35c76d15
Ian Anderson <ian.kirk.anderson@gmail.com>
2025-09-24 20:54:04 +0100

    Update port mappings in container startup script to make the web port accessible to other machines on the same network.

--- start.sh
+++ start.sh
@@ -11,8 +11,7 @@ podman run -it -d \
     --memory=5g \
     --memory-reservation=4g \
     -p 2222:22 \
-    -p 8080:8080 \
-    -p 3000:3000 \
+    -p 0.0.0.0:8100:8100 \
     --cap-add=CAP_AUDIT_WRITE \
     -v $HOME/.ssh/id_ed25519.pub:/home/developer/.ssh/authorized_keys \
     -v $HOME/.ssh/id_ed25519.pub:/home/developer/.ssh/id_ed25519.pub \
@@ -21,4 +20,3 @@ podman run -it -d \
     -v $CONTAINER_DIR/:/home/developer/development \
     shell-development:latest \
     /usr/sbin/sshd -D -e
-
